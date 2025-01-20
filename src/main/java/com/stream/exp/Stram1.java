1e855e5c7ddc5c398de964d754cd9ce3607661c3
Chen Ao <ao.chen02@hand-china.com>
2025-01-20 16:38:02 +0800

    init

---
+++ src/main/java/com/stream/exp/Stram1.java
@@ -0,0 +1,14 @@
+package com.stream.exp;
+
+/**
+ * <p>
+ * description： this is my first example
+ * </p>
+ *
+ * @author ao.chen02@hand-china.com
+ */
+public class Stram1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
